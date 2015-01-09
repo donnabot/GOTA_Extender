ce0ad7b1cd56f1f1389698aaca149a0a14fd570f
panayot-zhi <panayot_zhi@abv.bg>
2015-01-09 15:14:33 +0200

    Update version -release

--- GOTA_Extender.user.js
+++ GOTA_Extender.user.js
@@ -12,12 +12,12 @@
 // @exclude     http://gota-www.disruptorbeam.com/users/login*
 // @license     WTFPL (more at http://www.wtfpl.net/)
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require     https://greasyfork.org/scripts/5427-gota-extender-constants/code/GOTA_Extender_Constants.js?version=29418
+// @require     https://greasyfork.org/scripts/5427-gota-extender-constants/code/GOTA_Extender_Constants.js?version=31297
 // @require     https://greasyfork.org/scripts/5279-greasemonkey-supervalues/code/GreaseMonkey_SuperValues.js?version=20819
-// @resource 	custom https://greasyfork.org/scripts/5426-gota-extender-custom/code/GOTA_Extender_Custom.js?version=29284
-// @resource    auxiliary https://greasyfork.org/scripts/5618-gota-extender-auxiliary/code/GOTA_Extender_Auxiliary.js?version=29117
-// @resource    original https://greasyfork.org/scripts/6702-gota-extender-original/code/GOTA_Extender_Original.js?version=29118
-// @version     6.1.3
+// @resource 	custom https://greasyfork.org/scripts/5426-gota-extender-custom/code/GOTA_Extender_Custom.js?version=31296
+// @resource    auxiliary https://greasyfork.org/scripts/5618-gota-extender-auxiliary/code/GOTA_Extender_Auxiliary.js?version=31298
+// @resource    original https://greasyfork.org/scripts/6702-gota-extender-original/code/GOTA_Extender_Original.js?version=31299
+// @version     6.2.2
 // @grant       unsafeWindow
 // @grant       GM_getValue
 // @grant       GM_setValue
