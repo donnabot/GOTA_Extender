c43bfa2738c10b4b28901e245329d33dd6597f6c
donnabot <donnabotwin69@gmail.com>
2015-01-19 00:12:09 +1100

    Refixing the bad fixing of conflicts

--- GOTA_Extender.user.js
+++ GOTA_Extender.user.js
@@ -11,11 +11,9 @@
 // @exclude     http://gota.disruptorbeam.com/users/login*
 // @exclude     http://gota-www.disruptorbeam.com/users/login*
 // @license     WTFPL (more at http://www.wtfpl.net/)
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
-// @require     https://greasyfork.org/scripts/7491-donnabot-s-gota-extender-constants/code/Donnabot's%20GOTA_Extender_Constants.js?version=31941
 // @require     http://momentjs.com/downloads/moment.min.js
 // @require     http://code.jquery.com/jquery-2.1.3.min.js
-// @require     https://greasyfork.org/scripts/5427-gota-extender-constants/code/GOTA_Extender_Constants.js?version=31297
+// @require     https://greasyfork.org/scripts/7491-donnabot-s-gota-extender-constants/code/Donnabot's%20GOTA_Extender_Constants.js?version=31941
 // @require     https://greasyfork.org/scripts/5279-greasemonkey-supervalues/code/GreaseMonkey_SuperValues.js?version=20819
 // @resource 	custom https://greasyfork.org/scripts/7492-donnabot-s-gota-extender-custom/code/Donnabot's%20GOTA_Extender_Custom.js?version=31942
 // @resource    auxiliary https://greasyfork.org/scripts/7490-donnabot-s-gota-extender-auxiliary/code/Donnabot's%20GOTA_Extender_Auxiliary.js?version=31940
