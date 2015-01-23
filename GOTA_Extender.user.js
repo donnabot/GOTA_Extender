4b14d3e466667c4793f5b969f34d31900ad065be
donnabot <donnabotwin69@gmail.com>
2015-01-24 08:09:07 +1100

    Try with a delay

--- GOTA_Extender.user.js
+++ GOTA_Extender.user.js
@@ -20,7 +20,7 @@
 // @resource    auxiliary https://greasyfork.org/scripts/7490-donnabot-s-gota-extender-auxiliary/code/Donnabot's%20GOTA_Extender_Auxiliary.js?version=33081
 // @resource    original https://greasyfork.org/scripts/7493-donnabot-s-gota-extender-original/code/Donnabot's%20GOTA_Extender_Original.js?version=33084
 // @resource    production https://greasyfork.org/scripts/7612-donnabots-gota-extender-production/code/Donnabots_GOTA_Extender_Production.js?version=33080
-// @version     0.0.14
+// @version     0.0.15
 // @grant       unsafeWindow
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -160,6 +160,13 @@ function initialize() {
 
         // Sort player inventory
         unsafeWindow.sort();
+
+	//autobrute
+	if(options.autoBrute){
+		var brutedelay = 5000;
+		unsafeWindow.setTimeout(bruteSendAll,brutedelay);
+		log("Auto Bruting.");
+	}
 
 	log('Initialized. Happy hacking.');
         inform("Initialized.");
@@ -675,13 +682,6 @@ function acceptAllFavors() {
                 }
             } else {
                 log("All favors have been claimed.");
-
-	//autobrute in another terrible spot that might work
-	if(options.autoBrute){
-		unsafeWindow.bruteSendAll();
-		log("Auto Bruting.");
-	}
-
             }
         }
     });
