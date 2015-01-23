00a70d786ae6403de120f870458067c347e98090
donnabot <donnabotwin69@gmail.com>
2015-01-24 07:46:06 +1100

    Testing

--- GOTA_Extender.user.js
+++ GOTA_Extender.user.js
@@ -20,7 +20,7 @@
 // @resource    auxiliary https://greasyfork.org/scripts/7490-donnabot-s-gota-extender-auxiliary/code/Donnabot's%20GOTA_Extender_Auxiliary.js?version=33081
 // @resource    original https://greasyfork.org/scripts/7493-donnabot-s-gota-extender-original/code/Donnabot's%20GOTA_Extender_Original.js?version=33084
 // @resource    production https://greasyfork.org/scripts/7612-donnabots-gota-extender-production/code/Donnabots_GOTA_Extender_Production.js?version=33080
-// @version     0.0.11
+// @version     0.0.12
 // @grant       unsafeWindow
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -163,17 +163,13 @@ function initialize() {
 
 	log('Initialized. Happy hacking.');
         inform("Initialized.");
-
-	//autobrute
-	autobruteall();
 	
     } catch (e) {
         error("Fatal error, initialization failed: " + e);
         inform("Fatal error, initialization failed: " + e);
+    }
 	//autobrute
 	autobruteall();
-    }
-
 }
 // <-- End of initialization
 
