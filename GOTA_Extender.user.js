8fdf6b3dfd414831310edec358bbdcc1dae93e4b
donnabot <donnabotwin69@gmail.com>
2015-01-24 08:15:28 +1100

    Testing

--- GOTA_Extender.user.js
+++ GOTA_Extender.user.js
@@ -20,7 +20,7 @@
 // @resource    auxiliary https://greasyfork.org/scripts/7490-donnabot-s-gota-extender-auxiliary/code/Donnabot's%20GOTA_Extender_Auxiliary.js?version=33081
 // @resource    original https://greasyfork.org/scripts/7493-donnabot-s-gota-extender-original/code/Donnabot's%20GOTA_Extender_Original.js?version=33084
 // @resource    production https://greasyfork.org/scripts/7612-donnabots-gota-extender-production/code/Donnabots_GOTA_Extender_Production.js?version=33080
-// @version     0.0.15
+// @version     0.0.potato
 // @grant       unsafeWindow
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -164,7 +164,7 @@ function initialize() {
 	//autobrute
 	if(options.autoBrute){
 		var brutedelay = 5000;
-		unsafeWindow.setTimeout(bruteSendAll,brutedelay);
+		unsafeWindow.setTimeout(function() { unsafeWindow.bruteSendAll(); },brutedelay);
 		log("Auto Bruting.");
 	}
 
