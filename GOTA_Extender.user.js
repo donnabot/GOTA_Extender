c0938bfb543932fef84e048109d526ad0aca5b2e
panayot-zhi <panayot_zhi@abv.bg>
2015-01-24 15:54:26 +0200

    Update version - release

--- GOTA_Extender.user.js
+++ GOTA_Extender.user.js
@@ -20,7 +20,7 @@
 // @resource    auxiliary https://greasyfork.org/scripts/5618-gota-extender-auxiliary/code/GOTA_Extender_Auxiliary.js?version=33251
 // @resource    original https://greasyfork.org/scripts/6702-gota-extender-original/code/GOTA_Extender_Original.js?version=31299
 // @resource    production https://greasyfork.org/scripts/7611-gota-extender-production/code/GOTA_Extender_Production.js?version=33250
-// @version     6.6.6
+// @version     6.7.1
 // @grant       unsafeWindow
 // @grant       GM_getValue
 // @grant       GM_setValue
